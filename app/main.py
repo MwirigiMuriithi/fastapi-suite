b4f150daebfebca0565f9dbc2ac63ae5f442e954
MwirigiMuriithi <learnerviner@gmail.com>
2025-01-23 11:26:39 +0300

    Updated main.py to include task routes

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 from app.api.v1.auth import router as auth_router
+from app.api.v1 import task
 
 app = FastAPI()
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
+app.include_router(task.router, prefix="/tasks", tags=["tasks"])
