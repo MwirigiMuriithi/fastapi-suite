85470417ec25b74ff625a33346047d2da0e00aa1
MwirigiMuriithi <learnerviner@gmail.com>
2025-01-23 11:26:39 +0300

    Implemented business logic for tasks in task.py

---
+++ app/services/task.py
@@ -0,0 +1,23 @@
+# app/services/task.py
+from sqlalchemy.orm import Session
+from app.db import crud
+from app.schemas.task import TaskCreate, TaskUpdate
+
+class TaskService:
+    def __init__(self, db: Session):
+        self.db = db
+
+    def create_task(self, task: TaskCreate):
+        return crud.create_task(self.db, task)
+
+    def get_tasks(self, skip: int = 0, limit: int = 100):
+        return crud.get_tasks(self.db, skip, limit)
+
+    def get_task(self, task_id: int):
+        return crud.get_task(self.db, task_id)
+
+    def update_task(self, task_id: int, task: TaskUpdate):
+        return crud.update_task(self.db, task_id, task)
+
+    def delete_task(self, task_id: int):
+        return crud.delete_task(self.db, task_id)
