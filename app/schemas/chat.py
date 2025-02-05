7761fb591b5d1a4a3ce948e2a655e1e245fedb20
MwirigiMuriithi <learnerviner@gmail.com>
2025-02-05 21:23:11 +0300

    Created Pydantic schemas to validate chat message input and output

---
+++ app/schemas/chat.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from typing import List
+
+class ChatMessageBase(BaseModel):
+    message: str
+
+class ChatMessageIn(ChatMessageBase):
+    receiver_id: int
+
+class ChatMessageOut(ChatMessageBase):
+    sender_id: int
+    receiver_id: int
+    timestamp: int
+
+    class Config:
+        orm_mode = True
