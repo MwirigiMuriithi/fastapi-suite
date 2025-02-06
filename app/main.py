de760dafac72e7cad90e16d6f7ccd77150bc8bac
MwirigiMuriithi <learnerviner@gmail.com>
2025-02-06 23:58:53 +0300

    Updated main.py to include blog post routes

--- app/main.py
+++ app/main.py
@@ -3,9 +3,13 @@ from app.api.v1.auth import router as auth_router
 from app.api.v1 import task
 from app.api.v1 import note
 from app.api.v1 import chat
+from app.api.v1 import blog
+
 
 app = FastAPI()
 
+
+app.include_router(blog.router, prefix="/blogs", tags=["blogs"])
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(task.router, prefix="/tasks", tags=["tasks"])
 app.include_router(note.router, prefix="/notes", tags=["notes"])
