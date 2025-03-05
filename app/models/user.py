e575bcf1e839f07287b476cc605ebeebf9520529
MwirigiMuriithi <learnerviner@gmail.com>
2025-03-05 09:58:40 +0300

    feat: add blogs relationship to User model to support blog associations

--- app/models/user.py
+++ app/models/user.py
@@ -1,3 +1,15 @@
+# from sqlalchemy import Column, Integer, String
+# from sqlalchemy.orm import relationship
+# from app.db.session import Base
+
+# class User(Base):
+#     __tablename__ = "users"
+    
+#     id = Column(Integer, primary_key=True, index=True)
+#     username = Column(String, unique=True, index=True)
+#     email = Column(String, unique=True, index=True)
+#     hashed_password = Column(String)
+
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from app.db.session import Base
@@ -9,3 +21,5 @@ class User(Base):
     username = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
+    
+    blogs = relationship("Blog", back_populates="owner", cascade="all, delete-orphan")
